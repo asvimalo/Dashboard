1b255ad9cbb2937e1423fd0a5de4a9bcffb27209
asvimalo <asvimalo@gmail.com>
2017-09-10 18:54:53 +0200

    ProjectsController done

---
+++ Dashboard.API/Controllers/PicturesController.cs
@@ -0,0 +1,117 @@
+﻿using AutoMapper;
+using Dashboard.API.EF.IRepository;
+using Dashboard.Data.Entities;
+using Dashboard.Data.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dashboard.API.Controllers
+{
+  
+    [Route("api/dashboard/[controller]")]
+    public class PicturesController : Controller
+    {
+        public IRepository<Picture> _repo;
+        private ILogger<PicturesController> _logger;
+
+        public PicturesController(IRepository<Picture> repo,
+            ILogger<PicturesController> logger)
+        {
+            _repo = repo;
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // GET api/values
+        [HttpGet("")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var result = await _repo.GetAll();
+
+                return Ok(Mapper.Map<IEnumerable<PictureViewModel>>(result));
+            }
+            catch (Exception ex)
+            {
+                // LOGGING TODO
+                _logger.LogError($"Exception thrown while getting Pictures: {ex}");
+                return BadRequest($"Error ocurred");
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var result = await _repo.Get(id);
+                return Ok(Mapper.Map<PictureViewModel>(result));
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Exception thrown while getting Picture: {ex}");
+                return BadRequest($"Error ocurred");
+            }
+
+        }
+
+        // POST api/values
+        [HttpPost("")]
+        public async Task<IActionResult> Post([FromBody]PictureViewModel picture)
+        {
+            if (ModelState.IsValid)
+            {
+                var newPicture = Mapper.Map<Picture>(picture);
+                _repo.Add(newPicture);
+                if (await _repo.SaveChangesAsync())
+                {
+                    return Created($"api/dashboard/projects/{picture.Title}", Mapper.Map<PictureViewModel>(newPicture));
+                }
+            }
+            return BadRequest("Failed to save changes to the database");
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody]PictureViewModel pictureVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var pictureFromRepo = await _repo.Get(id);
+                Mapper.Map(pictureVM, pictureFromRepo);
+                var pictureUpdated = _repo.Update(pictureFromRepo);
+                if (!await _repo.SaveChangesAsync())
+                {
+                    _logger.LogError($"Thrown exception when updating");
+                    BadRequest("Something when wrong while updating");
+                }
+                return Ok(Mapper.Map<PictureViewModel>(pictureUpdated));
+            }
+            return BadRequest("Error occured");
+
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var pictureToDel = await _repo.Get(id);
+            _repo.Delete(pictureToDel);
+
+            if (await _repo.SaveChangesAsync())
+                return Ok($"Picture deleted!");
+            else
+                return BadRequest($"Picture {pictureToDel.Title} wasn't deleted!");
+        }
+    }
+
+}
--- Dashboard.API/Controllers/ProjectsController.cs
+++ Dashboard.API/Controllers/ProjectsController.cs
@@ -1,24 +1,26 @@
-﻿using System;
+﻿using AutoMapper;
+using Dashboard.API.EF.IRepository;
+using Dashboard.Data.Entities;
+using Dashboard.Data.ViewModel;
+using Dashboard.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Dashboard.API.EF.IRepository;
-using Dashboard.Data.Entities;
-using Dashboard.Data.ViewModels;
-using AutoMapper;
-using Microsoft.Extensions.Logging;
 
 namespace Dashboard.API.Controllers
 {
 
     [Route("api/dashboard/[controller]")]
-    public class CommitmentController : Controller
+    public class ProjectsController : Controller
     {
-        public IRepository<Commitment> _repo;
-        private ILogger<CommitmentController> _logger;
+        public IRepository<Project> _repo;
+        private ILogger<ProjectsController> _logger;
 
-        public CommitmentController(IRepository<Commitment> repo, ILogger<CommitmentController> logger)
+        public ProjectsController(IRepository<Project> repo, 
+            ILogger<ProjectsController> logger)
         {
             _repo = repo;
             _logger = logger;
@@ -35,7 +37,7 @@ namespace Dashboard.API.Controllers
             {
                 var result = await _repo.GetAll();
 
-                return Ok(Mapper.Map<IEnumerable<CommitmentViewModel>>(result));
+                return Ok(Mapper.Map<IEnumerable<ProjectViewModel>>(result));
             }
             catch (Exception ex)
             {
@@ -52,7 +54,7 @@ namespace Dashboard.API.Controllers
             try
             {
                 var result = await _repo.Get(id);
-                return Ok(Mapper.Map<CommitmentViewModel>(result));
+                return Ok(Mapper.Map<ProjectViewModel>(result));
             }
             catch (Exception ex)
             {
@@ -65,15 +67,15 @@ namespace Dashboard.API.Controllers
 
         // POST api/values
         [HttpPost("")]
-        public async Task<IActionResult> Post([FromBody]CommitmentViewModel commitment)
+        public async Task<IActionResult> Post([FromBody]ProjectViewModel project)
         {
             if (ModelState.IsValid)
             {
-                var newCommitment = Mapper.Map<Commitment>(commitment);
-                _repo.Add(newCommitment);
+                var newProject = Mapper.Map<Project>(project);
+                _repo.Add(newProject);
                 if (await _repo.SaveChangesAsync())
                 {
-                    return Created($"api/dashboard/commitments/{commitment.Name}", Mapper.Map<CommitmentViewModel>(newCommitment));
+                    return Created($"api/dashboard/projects/{project.Title}", Mapper.Map<ProjectViewModel>(newProject));
                 }
             }
             return BadRequest("Failed to save changes to the database");
@@ -81,34 +83,35 @@ namespace Dashboard.API.Controllers
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> Put(int id, [FromBody]CommitmentViewModel commitmentVM)
+        public async Task<IActionResult> Put(int id, [FromBody]ProjectViewModel projectVM)
         {
             if (ModelState.IsValid)
             {
-                var commiFromRepo = await _repo.Get(id);
-                Mapper.Map(commitmentVM, commiFromRepo);
-                var commitUpdated =  _repo.Update(commiFromRepo);
+                var projectFromRepo = await _repo.Get(id);
+                Mapper.Map(projectVM, projectFromRepo);
+                var projectUpdated = _repo.Update(projectFromRepo);
                 if (!await _repo.SaveChangesAsync())
                 {
                     _logger.LogError($"Thrown exception when updating");
                     BadRequest("Something when wrong while updating");
                 }
-                return Ok(Mapper.Map<CommitmentViewModel>(commitUpdated)); 
+                return Ok(Mapper.Map<ProjectViewModel>(projectUpdated));
             }
             return BadRequest("Error occured");
-            
+
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
-            var commiToDel = _repo.Get(id);
-
+            var projectToDel = await _repo.Get(id);
+            _repo.Delete(projectToDel);
             if (await _repo.SaveChangesAsync())
-                return Ok($"Commitment deleted!");
+                return Ok($"Project deleted!");
             else
-                return BadRequest($"Commitment {commiToDel.Result.Name} wasn't deleted!");
+                return BadRequest($"Project {projectToDel.Title} wasn't deleted!");
         }
     }
+
 }
