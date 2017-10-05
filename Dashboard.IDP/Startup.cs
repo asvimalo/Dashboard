62d0b43d7ac97bb4804bd02c02607126057ea3ba
asvimalo <asvimalo@gmail.com>
2017-10-05 10:23:33 +0200

    IdentityServer4 installed

---
+++ Dashboard.IDP/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Dashboard.IDP
+{
+    public class Startup
+    {
+       
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            app.UseDeveloperExceptionPage();
+
+            app.UseIdentityServer();
+        }
+    }
+}
