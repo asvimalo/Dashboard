5e0a115cabee608a7cf51a87973296c596d94bff
asvimalo <asvimalo@gmail.com>
2017-11-02 20:02:23 +0100

    employee add component

---
+++ Dashboard.Web/wwwroot/js/app/employee/employee-add/employee-add.component.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    "use strict";
+    angular.module("employeeAdd")
+        .component("employeeAdd", {
+            templateUrl: "/js/app/employee/employee-add/employee-add.template.html",
+            controller: function EmployeeListController($http) {
+                var holder = this;
+
+                holder.employees = [];
+
+                holder.newEmployee = {};
+                console.log("inside employees Controller ");
+                holder.errorMessage = "";
+                holder.isBusy = true;
+
+                holder.addEmployee = function (employee) {
+                    holder.isBusy = true;
+                    holder.errorMessage = "";
+
+                    $http.post("http://localhost:8899/api/dashboard/employees", employee)
+                        .then(function (response) {
+                            //success
+                            //holder.employees.push(response.data);
+                            holder.newEmployee = {};
+                            $location.path('#/employees');
+                        }, function () {
+                            //failure
+                            holder.errorMessage = "Failure to save new employee";
+                        })
+                        .finally(function () {
+                            holder.isBusy = false;
+                        });
+
+                };
+            }
+            
+        });
+      
+})();
