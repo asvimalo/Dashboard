c1976d8131e9536238285a2eb6f38ba1d99d78c0
Katrina Rosales <katrinarosales@live.com>
2017-11-04 10:28:01 +0100

    Pushed projectadd

--- Dashboard.Web/wwwroot/js/app/project/project-add/project-add.component.js
+++ Dashboard.Web/wwwroot/js/app/project/project-add/project-add.component.js
@@ -4,41 +4,50 @@
     angular.module("projectAdd")
         .component("projectAdd", {
             templateUrl: "/js/app/project/project-add/project-add.template.html",
-            controller: function ProjectAddController($http, $routeProvider) {
+            controller: function ProjectAddController($http, $scope) {
                 var self = this;
-
-                //var self.project = {};
-                self.animationsEnabled = true;
-
+                 
                 // Putting all my Clients in this array.
-                self.clients = [];
+                self.clients = []; 
+                // Putting all my Employees in this array.
+                self.employees = [];
 
+                 
                 // Getting my Clients
                 $http.get('http://localhost:8899/api/dashboard/clients').then(function (response) {
                     angular.copy(response.data, self.clients);
                 });
 
-                self.addProject = function () {
+                // Getting my Employee
+                $http.get('http://localhost:8899/api/dashboard/employees').then(function (response) {
+                    angular.copy(response.data, self.employees);
+                });
+
+                $scope.addProject = function () {
+                    console.log("in the addProject function");
                     self.isBusy = true;
                     self.errorMessage = "";
 
-                    $http.post("http://localhost:8899/api/dashboard/projects", $routeProvider.project)
+                    var data = { "projectName": self.project.projectName, "startDate": self.project.startDate, "stopDate": self.project.stopDate, "timeBudget": self.project.timeBudget, "ClientId": $scope.formInfo.clients.clientId, "EmployeeId": $scope.formInfo.employees.employeeId, "notes": self.project.notes };
+                    var dataTmp = JSON.stringify(data);
+
+                    $http.post("http://localhost:8899/api/dashboard/projects", dataTmp)
                         .then(function (response) {
-                            //success
-                            self.projects.push(response.data);
-                            self.newProject = {};
+                            console.log("Response from server api" + response.data);
 
                         }, function () {
                             //failure
+                            console.log("failure");
                             self.errorMessage = "Failure to save new project";
                         })
                         .finally(function () {
+                            console.log("finally");
                             self.isBusy = false;
                         });
 
                 };
             }
-        }
+        });
       
 })(); 
 
