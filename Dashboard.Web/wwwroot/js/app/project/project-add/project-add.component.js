19bc0209fb95e709dc3ba49873ad370dd9cda6e3
Katrina Rosales <katrinarosales@live.com>
2017-11-08 19:07:48 +0100

    ProjectAdd : Validation : DONE Add Multiple Employees : DONE

--- Dashboard.Web/wwwroot/js/app/project/project-add/project-add.component.js
+++ Dashboard.Web/wwwroot/js/app/project/project-add/project-add.component.js
@@ -7,29 +7,23 @@
             controller: function ProjectAddController($http, $scope) {
                 var self = this;
                  
-                // Putting all my Clients in this array.
                 self.clients = []; 
-                // Putting all my Employees in this array.
                 self.employees = [];
-
-                 
-                // Getting my Clients
+                
                 $http.get('http://localhost:8890/api/dashboard/clients').then(function (response) {
                     angular.copy(response.data, self.clients);
                 });
 
-                // Getting my Employee
                 $http.get('http://localhost:8890/api/dashboard/employees').then(function (response) {
                     angular.copy(response.data, self.employees);
                 });
-
-                // Add Project Function
+                 
                 $scope.addProject = function () {
                     console.log("in the addProject function");
                     self.isBusy = true;
                     self.errorMessage = "";
 
-                    var data = { "projectName": self.project.projectName, "startDate": new Date(self.project.startDate).toLocaleDateString(), "stopDate": new Date(self.project.stopDate).toLocaleDateString(), "timeBudget": self.project.timeBudget, "ClientId": $scope.formInfo.clients.clientId, "EmployeeId": $scope.formInfo.employees.employeeId, "notes": self.project.notes };
+                    var data = { "projectName": self.project.projectName, "startDate": new Date(self.project.startDate).toLocaleDateString(), "stopDate": new Date(self.project.stopDate).toLocaleDateString(), "timeBudget": self.project.timeBudget, "ClientId": $scope.formInfo.clients.clientId, "Employees": $scope.formInfo.employees, "notes": self.project.notes };
                     var dataTmp = JSON.stringify(data);
 
                     $http.post("http://localhost:8890/api/dashboard/projects", dataTmp)
@@ -37,7 +31,6 @@
                             console.log("Response from server api" + response.data);
 
                         }, function () {
-                            //failure
                             console.log("failure");
                             self.errorMessage = "Failure to save new project";
                         })
@@ -47,9 +40,9 @@
                             window.location.reload();
 
                         });
-
-
+                                        
                 };
+                 
                 
             }
         });
