69f4ea65db21b8614da880be2474c1a6d82c2104
asvimalo <asvimalo@gmail.com>
2017-11-17 12:44:14 +0100

    Repos created

---
+++ Dashboard.Web/wwwroot/js/app/services/repoAssignments.service.js
@@ -0,0 +1,96 @@
+﻿
+var repoAssignments = function ($http) {
+
+    var getAssignments = function () {
+        return $http.get('http://localhost:8890/api/dashboard/assignments')
+            .then(function (response) {
+
+                return response.data;
+
+            }, function (error) {
+
+                console.log("didn't get assignments: " + error.message);
+            })
+            .finally(function () {
+
+                console.log("Finally...??");
+            });
+    };
+
+    var getAssignmentById = function (id) {
+        return $http.get("http://localhost:8890/api/dashboard/assignments/", id)
+            .then(function (response) {
+                return response.data;
+            }, function (error) {
+
+                console.log("didn't get assignment: " + error.message);
+            })
+            .finally(function () {
+
+                console.log("Finally...??");
+            });
+    };
+    var getAssignmentByName = function (name) {
+        return $http.get("http://localhost:8890/api/dashboard/assignments/", name)
+            .then(function (response) {
+                return response.data;
+            }, function (error) {
+
+                console.log("didn't get assignment: " + error.message);
+            })
+            .finally(function () {
+
+                console.log("Finally...??");
+            });
+    };
+    var addAssignment = function (assignment) {
+        $http.post('http://localhost:8890/api/dashboard/assignments', assignment)
+            .then(function (response) {
+                return response.data;
+            }, function (error) {
+
+                console.log("didn't add assignment: " + error.message);
+            })
+            .finally(function () {
+
+                console.log("Finally...??");
+            });
+    };
+    var updateAssignment = function (assignment) {
+        $http.put('http://localhost:8890/api/dashboard/assignments', assignment)
+            .then(function (response) {
+                return response.data;
+            }, function (error) {
+
+                console.log("didn't update employee: " + error.message);
+            })
+            .finally(function () {
+
+                console.log("Finally...??");
+            });
+    };
+    var deleteAssignment = function (id) {
+        $http.delete('http://localhost:8890/api/dashboard/assignments', id)
+            .then(function (response) {
+                return response.data;
+            }, function (error) {
+
+                console.log("didn't delete employee: " + error.message);
+            })
+            .finally(function () {
+
+                console.log("Finally...??");
+            });
+    };
+    return {
+        getAll: getAssignments,
+        get: getAssignmentById,
+        getByName: getAssignmentByName,
+        add: addAssignment,
+        update: updateAssignment,
+        delete: deleteAssignment
+    };
+};
+
+var module = angular.module('app-dashboard');
+module.factory('repoAssignments', repoAssignments);
