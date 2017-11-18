b6b837af19f3f2faeddf2a38b78e8e2d1153c585
Katrina Rosales <katrinarosales@live.com>
2017-11-18 12:01:12 +0100

    Phase Delete Is Working. Snabb lösning fixat Phase Add : Start
    Fixed repoPhases

--- Dashboard.Web/wwwroot/js/app/phase/phase-add/phase-add.component.js
+++ Dashboard.Web/wwwroot/js/app/phase/phase-add/phase-add.component.js
@@ -3,13 +3,28 @@
     angular.module("phaseAdd")
         .component("phaseAdd", {
             templateUrl: "/js/app/phase/phase-add/phase-add.template.html",
-            controller: function PhaseListController($http) {
+            controller: function PhaseListController($http, $scope, repoPhases, $location)
+            {
                 var self = this;
-                //self.orderProp = 'projectName';
                 self.phases = [];
-                $http.get('http://localhost:8890/api/dashboard/phases').then(function (response) {
-                    angular.copy(response.data, self.phases);
-                });
+
+                $scope.addPhase = function () {
+                    var data = { "phaseName": self.phase.phaseName, "startDate": self.phase.startDate, "endDate": self.phase.endDate, "timeBudget": self.phase.timeBudget, "projectId": self.projectId };
+                    var dataTmp = JSON.stringify(data);
+                    
+
+                    // Http Post for Location 
+                    repoPhases.add(dataTmp).then(function (response) {
+                        console.log("Response from server api" + response);
+
+
+                    }, function (error) {
+                        //failure
+                        console.log("failure");
+                        self.errorMessage = "Failure to save new project";
+                        window.location.reload();
+                    });
+                }
             }
             
         });
