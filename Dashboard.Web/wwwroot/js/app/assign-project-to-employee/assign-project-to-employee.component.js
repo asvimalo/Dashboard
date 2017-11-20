97eab658bd705aff658262f71f788525567aa397
miamano <frinyo@hotmail.com>
2017-11-20 21:37:00 +0100

    APTE: buggfix

--- Dashboard.Web/wwwroot/js/app/assign-project-to-employee/assign-project-to-employee.component.js
+++ Dashboard.Web/wwwroot/js/app/assign-project-to-employee/assign-project-to-employee.component.js
@@ -7,7 +7,6 @@
                 $http,
                 $scope,
                 $location,
-                repoEmployees,
                 repoAssignments
             ) {
                 //$routeProvider
