4356d9f67fb2af7de128dae27f8dc8c3c50790be
Katrina Rosales <katrinarosales@live.com>
2017-12-05 16:28:02 +0100

    Tog bort reload

--- Dashboard.Web/wwwroot/js/app/allproject/all-projects.component.js
+++ Dashboard.Web/wwwroot/js/app/allproject/all-projects.component.js
@@ -296,7 +296,6 @@
                     $(".projectName").click(function () {
                         var projectId = $(this).parent().attr('projectnbr');
                         window.location.assign("http://localhost:8899/#!/projects/project-details/" + projectId);
-                        window.location.reload();
                    }).eq(0);
 
                 } //end of function resetGantt() **********************************************************************************************************************************
